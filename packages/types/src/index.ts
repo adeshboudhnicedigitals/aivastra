284388c0ff4fa3eaab356fd2560f753cbf3e0168
praveenniceinteractive-cpu <praveenniceinteractive@gmail.com>
2026-07-30 22:07:08 +0530

    feat(types): add job taxonomy registry (JOB_SOURCE, WORKER_POOL)

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -6,6 +6,7 @@ export * from './chatbot.js';
 export * from './common.js';
 export * from './credits.js';
 export * from './dev.js';
+export * from './job-taxonomy.js';
 export * from './jobs.js';
 export * from './saree.js';
 export * from './tryon.js';
